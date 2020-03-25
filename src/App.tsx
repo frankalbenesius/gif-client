d752ba0a856ddaf7bd4d6e83be2c732b676cb476
Frank Albenesius <frankalbenesius@gmail.com>
2020-03-24 23:12:22 -0500

    Download webm.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
   const videoEl = useRef<HTMLVideoElement>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
 
   useEffect(() => {
+    const mediaRecorderOptions = { mimeType: "video/webm; codecs=vp9" };
     navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
       if (videoEl.current) {
         videoEl.current.srcObject = stream;
+        mediaRecorderRef.current = new MediaRecorder(
+          stream,
+          mediaRecorderOptions
+        );
+        mediaRecorderRef.current.ondataavailable = handleDataAvailable;
       }
     });
   }, []);
 
+  function handleRecordGif(e: React.MouseEvent) {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.start();
+      setTimeout(() => {
+        if (mediaRecorderRef.current) {
+          mediaRecorderRef.current.stop();
+        }
+      }, 3000);
+    }
+  }
+
+  const [blob, setBlob] = useState<Blob>();
+  function handleDataAvailable(event: BlobEvent) {
+    console.log("data-available");
+    setBlob(event.data);
+  }
+  useEffect(() => {
+    if (blob) {
+      var url = URL.createObjectURL(blob);
+      let a = document.createElement("a");
+      document.body.appendChild(a);
+      // a.style = "display: none";
+      a.href = url;
+      a.download = "test.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+    }
+  }, [blob]);
+
   return (
     <div>
       <video autoPlay ref={videoEl}></video>
-      <p>this is good shit</p>
+      <div>
+        <button onClick={handleRecordGif}>record gif</button>
+      </div>
     </div>
   );
 }
