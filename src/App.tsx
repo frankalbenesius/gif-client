e3c7b2ec9739804248769842f30d59359ccf6212
frankalbenesius <frankalbenesius@gmail.com>
2020-04-04 15:01:43 -0500

    480p constraints & css

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     if (blob) {
       fetch("https://franks-gif-api.herokuapp.com/gif", {
         method: "POST",
-        body: blob
-      }).then(res => {
-        res.blob().then(blob => {
+        body: blob,
+      }).then((res) => {
+        res.blob().then((blob) => {
           const timestamp = format(new Date(), "t");
           download(blob, `gif_${timestamp}.gif`, "image/gif");
         });
@@ -20,11 +20,42 @@ function App() {
   }, [blob]);
 
   return (
-    <div>
-      <video autoPlay ref={videoEl}></video>
+    <div style={{ maxWidth: "640px", margin: "0 auto", padding: "1rem" }}>
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "640px",
+          backgroundColor: "black",
+          position: "relative",
+          marginBottom: "0.25rem",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            paddingTop: "75%",
+          }}
+        ></div>
+        <video
+          style={{ top: 0, left: 0, position: "absolute", width: "100%" }}
+          autoPlay
+          ref={videoEl}
+        ></video>
+      </div>
       <div>
-        <button onClick={() => record()}>
-          record and download 3 second gif
+        <button
+          style={{
+            border: 0,
+            background: "green",
+            color: "white",
+            width: "100%",
+            padding: "0.5rem",
+            fontSize: "1.5rem",
+            textTransform: "uppercase",
+          }}
+          onClick={() => record()}
+        >
+          record gif
         </button>
       </div>
     </div>
@@ -33,23 +64,29 @@ function App() {
 
 export default App;
 
-
-type HookValues = [() => void, Blob | null]
-function useMediaRecorder(videoRef: React.RefObject<HTMLVideoElement>): HookValues {
+type HookValues = [() => void, Blob | null];
+function useMediaRecorder(
+  videoRef: React.RefObject<HTMLVideoElement>
+): HookValues {
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
 
   useEffect(() => {
     const mediaRecorderOptions = { mimeType: "video/webm; codecs=vp8" };
-    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-      if (videoRef.current) {
-        videoRef.current.srcObject = stream;
-        mediaRecorderRef.current = new MediaRecorder(
-          stream,
-          mediaRecorderOptions
-        );
-        mediaRecorderRef.current.ondataavailable = handleDataAvailable;
-      }
-    });
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: { width: 640, height: 480 } })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          mediaRecorderRef.current = new MediaRecorder(
+            stream,
+            mediaRecorderOptions
+          );
+          mediaRecorderRef.current.ondataavailable = handleDataAvailable;
+        }
+      })
+      .catch((e) => {
+        console.error("mediaDevices.getUserMedia failed with error:", e);
+      });
   }, [videoRef]);
 
   function record() {
@@ -69,5 +106,5 @@ function useMediaRecorder(videoRef: React.RefObject<HTMLVideoElement>): HookValu
     setBlob(event.data);
   }
 
-  return [record, blob]
+  return [record, blob];
 }
